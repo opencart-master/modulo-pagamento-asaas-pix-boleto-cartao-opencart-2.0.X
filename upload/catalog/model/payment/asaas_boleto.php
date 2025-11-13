8293965a66403d68b503b0f1f505b820e1a9cbad
deniscunha <ded_2004@hotmail.com>
2025-11-13 13:10:19 -0300

    update data frontend

--- upload/catalog/model/payment/asaas_boleto.php
+++ upload/catalog/model/payment/asaas_boleto.php
@@ -1,28 +1,17 @@
 <?php
-class ModelPaymentBankTransfer extends Model {
+class ModelPaymentAsaasBoleto extends Model {
 	public function getMethod($address, $total) {
-		$this->load->language('payment/bank_transfer');
-
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('bank_transfer_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
-
-		if ($this->config->get('bank_transfer_total') > 0 && $this->config->get('bank_transfer_total') > $total) {
-			$status = false;
-		} elseif (!$this->config->get('bank_transfer_geo_zone_id')) {
-			$status = true;
-		} elseif ($query->num_rows) {
-			$status = true;
-		} else {
-			$status = false;
-		}
+		$this->load->language('payment/asaas_boleto');
+        $status = $this->config->get('asaas_boleto_status');
 
 		$method_data = array();
 
 		if ($status) {
 			$method_data = array(
-				'code'       => 'bank_transfer',
+				'code'       => 'asaas_boleto',
 				'title'      => $this->language->get('text_title'),
 				'terms'      => '',
-				'sort_order' => $this->config->get('bank_transfer_sort_order')
+				'sort_order' => $this->config->get('asaas_boleto_sort_order')
 			);
 		}
 
